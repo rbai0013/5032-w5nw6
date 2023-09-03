dd7f9b5dc7bba0f420c1608e42c081e58de4ee2a
unknown <rbai0013@student.monash.edu>
2023-09-03 17:51:40 +1000

    update studio 6 task 1 with validation form

---
+++ MyFirstModelView/MyFirstModelView/Models/FormModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace MyFirstModelView.Models
+{
+    public class FormModel
+    {
+    }
+
+    public class FormOneViewModel 
+    { 
+        [Required(ErrorMessage = "First name is required!")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Minium length of 5 characters")]
+        [Display(Name = "First Name")] 
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required!")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Minium length of 5 characters")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } 
+    }
+}
